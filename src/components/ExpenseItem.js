3b317ffa1d2fff6f7b24b571c59b8f58e20e1ecf
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-09-28 11:40:04 +0200

    new Expense section

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,27 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  // let title = props.title;
+
+  const clickHandler = () => {
+    setTitle("updated yup");
+    // title = "Updated!";
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">R{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
