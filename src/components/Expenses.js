fa44d2d18fd73537ced6fc5dfe98a77d68f95351
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-09-30 12:12:10 +0200

    added state to expenseFilter

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -5,17 +5,18 @@ import "./Expenses.css";
 import { useState } from "react";
 
 function Expenses(props) {
-  const [changeFilterDate, setChangeFilterDate] = useState("2022");
+  const [filteredYear, setFilteredYear] = useState("2022");
 
   const filterChangeHandler = (selectedYear) => {
-    // setChangeFilterDate(event.target.value);
-    console.log(selectedYear);
+    setFilteredYear(selectedYear);
+    // console.log(filteredYear);
+    // console.log(selectedYear);
   };
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter value={changeFilterDate} onChangeFilter={filterChangeHandler} />
+        <ExpensesFilter value={filteredYear} onChangeFilter={filterChangeHandler} />
         <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
 
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
