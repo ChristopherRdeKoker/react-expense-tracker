edcb005c46a8a413929aa0705bbd56fa4c94605c
ChristopherRdeKoker <christopherdekoker@gmail.com>
2022-10-05 16:42:44 +0200

    added conditional for selected year = if no expense=> message

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -11,7 +11,7 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpense = props.items.filter((expense) => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
@@ -19,18 +19,11 @@ function Expenses(props) {
     <div>
       <Card className="expenses">
         <ExpensesFilter value={filteredYear} onChangeFilter={filterChangeHandler} />
-
-        {filteredExpense.map((expense) => (
-          <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-        ))}
-
-        {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
-
-        <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
-
-        <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
-
-        <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem> */}
+        {filteredExpenses.length === 0 ? (
+          <p>Sorry bud, no expenses found🐶</p>
+        ) : (
+          filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)
+        )}
       </Card>
     </div>
   );
